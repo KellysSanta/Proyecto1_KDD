b9c0a7859fedb54bedaf7292514f777393505277
KellysSanta <kellys.santa@correounivalle.edu.co>
2016-01-08 11:02:49 -0500

    interfaz

---
+++ js/ventasScripts.js
@@ -0,0 +1,14 @@
+
+var $form = $('.pregunta1'),
+	$button = $('#mostrarPregunta1');
+
+
+
+function mostrarFormulario()
+{
+	$form.slideToggle();/*Si esta oculto lo muestra, si esta visible lo oculta*/
+	return false; /*Quita el efecto que tiene por defecto la etiqueta a, ya que esta va a una url*/
+}
+
+//Eventos
+$button.click( mostrarFormulario );
