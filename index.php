bd8dc6a81624faaafbd69319d1e76b2b199ecf0e
KellysSanta <kellys.santa@correounivalle.edu.co>
2016-01-30 00:06:13 -0500

    extensión php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<?php $usuario=$_GET["usuario"];
+include "conexion.php";?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Bice-Bykes Bienvenido!</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+	<!--script src="js/less-1.3.3.min.js"></script-->
+	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+	
+	<link href="css/indexStyle.css" rel="stylesheet">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+
+  <!-- Fav and touch icons -->
+  <link rel="shortcut icon" href="img/icono.jpg">
+<link href='https://fonts.googleapis.com/css?family=Cabin' rel='stylesheet' type='text/css'>
+  
+	
+</head>
+
+<body>
+
+
+<div class="container">
+
+	<nav>
+		<ul class="menu_principal">
+			<li><a class="principal">Bici-Bykes</a></li>
+		</ul>
+
+	</nav>
+
+
+	<div class="contenido">
+		<div class="columna1">
+						<img  alt="140x140" width="100%" src="img/fondoInicio.jpeg">
+			<nav>
+				<ul class="menu_principal">
+					<li class="centro">
+						<a class="principal"  href="menuReportes.php">Entrar</a>
+					</li>
+				</ul>
+			</nav>
+
+			
+				</div>
+		</div>
+
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script type="text/javascript" src="js/mensajesscripts.js"></script>
+</body>
+</html>
+
+			
